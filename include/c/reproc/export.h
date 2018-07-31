b265c71dce7861df250ef4aba9b4da4608acc056
Daan De Meyer <daan.j.demeyer@gmail.com>
2018-07-31 15:42:16 +0200

    Fix export declarations not being empty for static library builds

--- include/c/reproc/export.h
+++ include/c/reproc/export.h
@@ -1,18 +1,22 @@
 #ifndef REPROC_EXPORT_H
 #define REPROC_EXPORT_H
 
-#if defined(_WIN32) && defined(REPROC_SHARED)
-#if defined(REPROC_BUILDING)
-#define REPROC_EXPORT __declspec(dllexport)
+#if defined(REPROC_SHARED)
+#  if defined(_WIN32)
+#    if defined(REPROC_BUILDING)
+#      define REPROC_EXPORT __declspec(dllexport)
+#    else
+#      define REPROC_EXPORT __declspec(dllimport)
+#    endif
+#  else
+#    if defined(REPROC_BUILDING)
+#      define REPROC_EXPORT __attribute__((visibility("default")))
+#    else
+#      define REPROC_EXPORT
+#    endif
+#  endif
 #else
-#define REPROC_EXPORT __declspec(dllimport)
-#endif
-#else
-#if defined(REPROC_BUILDING)
-#define REPROC_EXPORT __attribute__ ((visibility("default")))
-#else
-#define REPROC_EXPORT
-#endif
+#  define REPROC_EXPORT
 #endif
 
 #endif
