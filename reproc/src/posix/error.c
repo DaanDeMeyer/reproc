08b67910b6b02f6c9364b8ced0e30f01a486e573
Daan De Meyer <daan.j.demeyer@gmail.com>
2019-08-10 13:48:00 +0200

    Extend `reproc_strerror` on POSIX to return system error strings.
    If `reproc_strerror` is passed `REPROC_ERROR_SYSTEM`, it prints the
    platform-specific error string returned by calling `strerror` with the
    error returned by `reproc_system_error`.
    
    On Windows, we still return "system error" for now.

--- reproc/src/posix/error.c
+++ reproc/src/posix/error.c
@@ -1,10 +1,30 @@
 #include <reproc/error.h>
 
 #include <errno.h>
+#include <string.h>
 
 unsigned int reproc_system_error(void)
 {
   // Only positive numbers are valid `errno` values so casting to unsigned int
   // is safe.
   return (unsigned int) errno;
+}
+
+const char *reproc_strerror(REPROC_ERROR error)
+{
+  switch (error) {
+    case REPROC_SUCCESS:
+      return "success";
+    case REPROC_ERROR_WAIT_TIMEOUT:
+      return "wait timeout";
+    case REPROC_ERROR_STREAM_CLOSED:
+      return "stream closed";
+    case REPROC_ERROR_PARTIAL_WRITE:
+      return "partial write";
+    case REPROC_ERROR_SYSTEM:
+      // `reproc_system_error` returns `errno` which is always in `int` range.
+      return strerror((int) reproc_system_error());
+  }
+
+  return "unknown error";
 }
--- reproc/src/windows/error.c
+++ reproc/src/windows/error.c
@@ -6,3 +6,21 @@ unsigned int reproc_system_error(void)
 {
   return GetLastError();
 }
+
+const char *reproc_strerror(REPROC_ERROR error)
+{
+  switch (error) {
+    case REPROC_SUCCESS:
+      return "success";
+    case REPROC_ERROR_WAIT_TIMEOUT:
+      return "wait timeout";
+    case REPROC_ERROR_STREAM_CLOSED:
+      return "stream closed";
+    case REPROC_ERROR_PARTIAL_WRITE:
+      return "partial write";
+    case REPROC_ERROR_SYSTEM:
+      return "system error";
+  }
+
+  return "unknown error";
+}
