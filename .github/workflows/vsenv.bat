baf4e5a226b63cc518c2446d0b83f0a36cea9662
Daan De Meyer <daan.j.demeyer@gmail.com>
2019-10-20 12:55:55 +0200

    Use lowercase batch commands in vsenv.bat.

--- .github/workflows/vsenv.bat
+++ .github/workflows/vsenv.bat
@@ -1,6 +1,6 @@
 @echo off
 
-SET VSWHERE="C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere"
+set VSWHERE="C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere"
 
 :: See https://github.com/microsoft/vswhere/wiki/Find-VC
 for /f "usebackq delims=*" %%i in (`%VSWHERE% -latest -property installationPath`) do (
