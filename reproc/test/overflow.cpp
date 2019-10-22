1a2667b0868e683aab7f93e73376d1b03c5a7738
Daan De Meyer <daan.j.demeyer@gmail.com>
2019-10-22 20:09:23 +0200

    Add test against large amounts of child process output.

---
+++ reproc/test/overflow.cpp
@@ -0,0 +1,33 @@
+#include <doctest.h>
+
+#include <reproc/reproc.h>
+#include <reproc/sink.h>
+
+#include <array>
+
+TEST_CASE("overflow")
+{
+  reproc_t process;
+
+  REPROC_ERROR error = REPROC_SUCCESS;
+  INFO(reproc_error_string(error));
+
+  std::array<const char *, 2> argv = { RESOURCE_DIRECTORY "/overflow",
+                                       nullptr };
+
+  error = reproc_start(&process, argv.data(), nullptr, nullptr);
+  REQUIRE(!error);
+
+  char *output = nullptr;
+  error = reproc_drain(&process, reproc_sink_string, &output);
+  REQUIRE(!error);
+  REQUIRE(output != nullptr);
+
+  error = reproc_wait(&process, REPROC_INFINITE);
+  REQUIRE(!error);
+  REQUIRE(reproc_exit_status(&process) == 0);
+
+  reproc_destroy(&process);
+
+  free(output);
+}
