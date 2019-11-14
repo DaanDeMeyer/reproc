310bd523de905dfa16ea674f2efc1f34bcff17a1
Daan De Meyer <daan.j.demeyer@gmail.com>
2019-11-14 19:45:18 +0100

    Windows: Fix #26 and handle closed standard streams in `redirect`.

--- reproc/src/windows/redirect.c
+++ reproc/src/windows/redirect.c
@@ -19,61 +19,82 @@ static const struct pipe_options CHILD_OPTIONS = { .inherit = true,
 static const struct pipe_options PARENT_OPTIONS = { .inherit = false,
                                                     .nonblocking = true };
 
+static REPROC_ERROR
+redirect_pipe(HANDLE *parent, HANDLE *child, REPROC_STREAM stream)
+{
+  assert(parent);
+  assert(child);
+
+  return stream == REPROC_STREAM_IN
+             ? pipe_init(child, CHILD_OPTIONS, parent, PARENT_OPTIONS)
+             : pipe_init(parent, PARENT_OPTIONS, child, CHILD_OPTIONS);
+}
+
+static REPROC_ERROR
+redirect_inherit(HANDLE *parent, HANDLE *child, REPROC_STREAM stream)
+{
+  assert(parent);
+  assert(child);
+
+  *parent = NULL;
+  HANDLE *stream_handle = NULL;
+
+  DWORD stream_id = stream == REPROC_STREAM_IN
+                        ? STD_INPUT_HANDLE
+                        : stream == REPROC_STREAM_OUT ? STD_OUTPUT_HANDLE
+                                                      : STD_ERROR_HANDLE;
+  stream_handle = GetStdHandle(stream_id);
+  if (stream_handle == INVALID_HANDLE_VALUE) {
+    return REPROC_ERROR_SYSTEM;
+  }
+
+  if (stream_handle == NULL) {
+    return REPROC_ERROR_STREAM_CLOSED;
+  }
+
+  BOOL rv = DuplicateHandle(GetCurrentProcess(), stream_handle,
+                            GetCurrentProcess(), child, 0, true,
+                            DUPLICATE_SAME_ACCESS);
+
+  return rv == 0 ? REPROC_ERROR_SYSTEM : REPROC_SUCCESS;
+}
+
+static REPROC_ERROR
+redirect_discard(HANDLE *parent, HANDLE *child, REPROC_STREAM stream)
+{
+  assert(parent);
+  assert(child);
+
+  *parent = NULL;
+  DWORD mode = stream == REPROC_STREAM_IN ? GENERIC_READ : GENERIC_WRITE;
+
+  *child = CreateFile(DEVNULL, mode, FILE_NO_SHARE, &INHERIT_HANDLE,
+                      OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL,
+                      (HANDLE) FILE_NO_TEMPLATE);
+
+  return *child == INVALID_HANDLE_VALUE ? REPROC_ERROR_SYSTEM : REPROC_SUCCESS;
+}
+
 REPROC_ERROR
 redirect(HANDLE *parent,
          HANDLE *child,
          REPROC_STREAM stream,
          REPROC_REDIRECT type)
 {
-  assert(parent);
-  assert(child);
+  switch (type) {
 
-  *parent = NULL;
+    case REPROC_REDIRECT_PIPE:
+      return redirect_pipe(parent, child, stream);
 
-  switch (stream) {
+    case REPROC_REDIRECT_INHERIT:
+      REPROC_ERROR error = redirect_inherit(parent, child, stream);
+      // Discard if the corresponding parent stream is closed.
+      return error == REPROC_ERROR_STREAM_CLOSED
+                 ? redirect_discard(parent, child, stream)
+                 : error;
 
-    case REPROC_STREAM_IN:
-
-      switch (type) {
-        case REPROC_REDIRECT_PIPE:
-          return pipe_init(child, CHILD_OPTIONS, parent, PARENT_OPTIONS);
-
-        case REPROC_REDIRECT_INHERIT:
-          *child = GetStdHandle(STD_INPUT_HANDLE);
-          return *child == INVALID_HANDLE_VALUE ? REPROC_ERROR_SYSTEM
-                                                : REPROC_SUCCESS;
-
-        case REPROC_REDIRECT_DISCARD:
-          *child = CreateFile(DEVNULL, GENERIC_READ, FILE_NO_SHARE,
-                              &INHERIT_HANDLE, OPEN_EXISTING,
-                              FILE_ATTRIBUTE_NORMAL, (HANDLE) FILE_NO_TEMPLATE);
-          return *child == INVALID_HANDLE_VALUE ? REPROC_ERROR_SYSTEM
-                                                : REPROC_SUCCESS;
-      }
-
-      assert(false);
-      break;
-
-    case REPROC_STREAM_OUT:
-    case REPROC_STREAM_ERR:
-
-      switch (type) {
-        case REPROC_REDIRECT_PIPE:
-          return pipe_init(parent, PARENT_OPTIONS, child, CHILD_OPTIONS);
-
-        case REPROC_REDIRECT_INHERIT:
-          *child = GetStdHandle(stream == REPROC_STREAM_OUT ? STD_OUTPUT_HANDLE
-                                                            : STD_ERROR_HANDLE);
-          return *child == INVALID_HANDLE_VALUE ? REPROC_ERROR_SYSTEM
-                                                : REPROC_SUCCESS;
-
-        case REPROC_REDIRECT_DISCARD:
-          *child = CreateFile(DEVNULL, GENERIC_WRITE, FILE_NO_SHARE,
-                              &INHERIT_HANDLE, OPEN_EXISTING,
-                              FILE_ATTRIBUTE_NORMAL, (HANDLE) FILE_NO_TEMPLATE);
-          return *child == INVALID_HANDLE_VALUE ? REPROC_ERROR_SYSTEM
-                                                : REPROC_SUCCESS;
-      }
+    case REPROC_REDIRECT_DISCARD:
+      return redirect_discard(parent, child, stream);
   }
 
   assert(false);
