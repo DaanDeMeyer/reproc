1d8b0d0174740eb3cc04a30c191a329f13799137
Daan De Meyer <daan.j.demeyer@gmail.com>
2019-11-14 20:04:17 +0100

    POSIX: refactor `redirect`.

--- reproc/src/posix/redirect.c
+++ reproc/src/posix/redirect.c
@@ -11,66 +11,67 @@ static const char *DEVNULL = "/dev/null";
 static const struct pipe_options CHILD_OPTIONS = { .nonblocking = false };
 static const struct pipe_options PARENT_OPTIONS = { .nonblocking = true };
 
-REPROC_ERROR
-redirect(int *parent, int *child, REPROC_STREAM stream, REPROC_REDIRECT type)
+static REPROC_ERROR redirect_pipe(int *parent, int *child, REPROC_STREAM stream)
+{
+  assert(parent);
+  assert(child);
+
+  return stream == REPROC_STREAM_IN
+             ? pipe_init(child, CHILD_OPTIONS, parent, PARENT_OPTIONS)
+             : pipe_init(parent, PARENT_OPTIONS, child, CHILD_OPTIONS);
+}
+
+static REPROC_ERROR
+redirect_inherit(int *parent, int *child, REPROC_STREAM stream)
 {
   assert(parent);
   assert(child);
 
   *parent = 0;
+  FILE *file = stream == REPROC_STREAM_IN
+                   ? stdin
+                   : stream == REPROC_STREAM_OUT ? stdout : stderr;
 
-  int fd = -1;
-
-  if (type == REPROC_REDIRECT_INHERIT) {
-    // `REPROC_STREAM` => `FILE *`
-    FILE *file = stream == REPROC_STREAM_IN
-                     ? stdin
-                     : stream == REPROC_STREAM_OUT ? stdout : stderr;
-    fd = fileno(file);
-
-    // If we're inheriting a parent stream and the stream is not a valid stream
-    // (`fd == -1`), we interpret this as if the child process is inheriting a
-    // closed stream and discard all output from the child process stream.
-    if (fd == -1) {
-      type = REPROC_REDIRECT_DISCARD;
-    }
+  int fd = fileno(file);
+  if (fd == -1) {
+    return REPROC_ERROR_STREAM_CLOSED;
   }
 
-  switch (stream) {
+  *child = fcntl(fd, F_DUPFD_CLOEXEC, 0);
 
-    case REPROC_STREAM_IN:
+  return *child == -1 ? REPROC_ERROR_SYSTEM : REPROC_SUCCESS;
+}
 
-      switch (type) {
-        case REPROC_REDIRECT_PIPE:
-          return pipe_init(child, CHILD_OPTIONS, parent, PARENT_OPTIONS);
+static REPROC_ERROR
+redirect_discard(int *parent, int *child, REPROC_STREAM stream)
+{
+  assert(parent);
+  assert(child);
 
-        case REPROC_REDIRECT_INHERIT:
-          *child = fcntl(fd, F_DUPFD_CLOEXEC, 0);
-          return *child == -1 ? REPROC_ERROR_SYSTEM : REPROC_SUCCESS;
+  *parent = 0;
+  int mode = stream == REPROC_STREAM_IN ? O_RDONLY : O_WRONLY;
 
-        case REPROC_REDIRECT_DISCARD:
-          *child = open(DEVNULL, O_RDONLY | O_CLOEXEC);
-          return *child == -1 ? REPROC_ERROR_SYSTEM : REPROC_SUCCESS;
-      }
+  *child = open(DEVNULL, mode | O_CLOEXEC);
 
-      assert(false);
-      break;
+  return *child == -1 ? REPROC_ERROR_SYSTEM : REPROC_SUCCESS;
+}
 
-    case REPROC_STREAM_OUT:
-    case REPROC_STREAM_ERR:
+REPROC_ERROR
+redirect(int *parent, int *child, REPROC_STREAM stream, REPROC_REDIRECT type)
+{
+  switch (type) {
 
-      switch (type) {
-        case REPROC_REDIRECT_PIPE:
-          return pipe_init(parent, PARENT_OPTIONS, child, CHILD_OPTIONS);
+    case REPROC_REDIRECT_PIPE:
+      return redirect_pipe(parent, child, stream);
 
-        case REPROC_REDIRECT_INHERIT:
-          *child = fcntl(fd, F_DUPFD_CLOEXEC, 0);
-          return *child == -1 ? REPROC_ERROR_SYSTEM : REPROC_SUCCESS;
+    case REPROC_REDIRECT_INHERIT:;
+      REPROC_ERROR error = redirect_inherit(parent, child, stream);
+      return error == REPROC_ERROR_STREAM_CLOSED
+                 ? redirect_discard(parent, child, stream)
+                 : error;
 
-        case REPROC_REDIRECT_DISCARD:
-          *child = open(DEVNULL, O_WRONLY | O_CLOEXEC);
-          return *child == -1 ? REPROC_ERROR_SYSTEM : REPROC_SUCCESS;
-      }
+    case REPROC_REDIRECT_DISCARD:
+      return redirect_discard(parent, child, stream);
   }
 
   assert(false);
