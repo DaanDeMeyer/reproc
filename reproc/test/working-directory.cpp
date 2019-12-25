0b77ceef859772b84e4223148b949931b24dd0ed
Daan De Meyer <daan.j.demeyer@gmail.com>
2019-12-25 20:12:40 +0100

    reproc: Extend working-directory test.
    Until now, we didn't check whether the child process actually started in
    the specified working directory. To fix this, we renamed the noop
    resource to the working-directory resource which prints its working
    directory on stdout. We can then check this matches our expectations in
    the working-directory test.

--- reproc/test/working-directory.cpp
+++ reproc/test/working-directory.cpp
@@ -1,8 +1,12 @@
 #include <doctest.h>
 
 #include <reproc/reproc.h>
+#include <reproc/sink.h>
 
+#include <algorithm>
 #include <array>
+#include <cstring>
+#include <string>
 
 TEST_CASE("working-directory")
 {
@@ -14,7 +18,8 @@ TEST_CASE("working-directory")
   INFO(reproc_error_string(error));
 
   const char *working_directory = RESOURCE_DIRECTORY;
-  std::array<const char *, 2> argv{ RESOURCE_DIRECTORY "/noop", nullptr };
+  std::array<const char *, 2> argv{ RESOURCE_DIRECTORY "/working-directory",
+                                    nullptr };
 
   reproc_options options = {};
   options.working_directory = working_directory;
@@ -22,9 +27,18 @@ TEST_CASE("working-directory")
   error = reproc_start(process, argv.data(), options);
   REQUIRE(!error);
 
+  char *output = nullptr;
+  error = reproc_drain(process, reproc_sink_string, &output);
+  REQUIRE(!error);
+
+  std::replace(output, output + strlen(output), '\\', '/');
+  REQUIRE(std::string(output) == RESOURCE_DIRECTORY);
+
   error = reproc_wait(process, REPROC_INFINITE);
   REQUIRE(!error);
   REQUIRE(reproc_exit_status(process) == 0);
 
   reproc_destroy(process);
+
+  free(output);
 }
