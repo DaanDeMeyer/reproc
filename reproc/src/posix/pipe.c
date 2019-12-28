b8f4647178b8b3c3a46881820e92526b740d1ffb
Daan De Meyer <daan.j.demeyer@gmail.com>
2019-12-28 20:56:34 +0100

    reproc: Shorten comment.

--- reproc/src/posix/pipe.c
+++ reproc/src/posix/pipe.c
@@ -21,22 +21,6 @@ int pipe_init(int *read,
   int pipefd[2] = { -1, -1 };
   int r = -1;
 
-  // On POSIX systems, by default file descriptors are inherited by child
-  // processes when calling `exec`. To prevent unintended leaking of file
-  // descriptors to child processes, POSIX provides a function `fcntl` which can
-  // be used to set the `FD_CLOEXEC` flag which closes a file descriptor when
-  // `exec` (or one of its variants) is called. However, using `fcntl`
-  // introduces a race condition since any process spawned in another thread
-  // after a file descriptor is created (for example using `pipe`) but before
-  // `fcntl` is called to set `FD_CLOEXEC` on the file descriptor will still
-  // inherit that file descriptor.
-
-  // To get around this race condition we use the `pipe2` function when it
-  // is available which takes the `O_CLOEXEC` flag as an argument. This ensures
-  // the file descriptors of the created pipe are closed when `exec` is called.
-  // If `pipe2` is not available we fall back to calling `fcntl` to set
-  // `FD_CLOEXEC` immediately after creating a pipe.
-
 #if defined(__APPLE__)
   r = pipe(pipefd);
   if (r < 0) {
@@ -53,6 +37,8 @@ int pipe_init(int *read,
     goto cleanup;
   }
 #else
+  // `pipe2` with `O_CLOEXEC` avoids the race condition between `pipe` and
+  // `fcntl`.
   r = pipe2(pipefd, O_CLOEXEC);
   if (r < 0) {
     goto cleanup;
