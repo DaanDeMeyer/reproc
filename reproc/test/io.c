812ba5f74526b6d8370288b9dd33bd1a983606e4
Daan De Meyer <daan.j.demeyer@gmail.com>
2020-01-15 21:03:12 +0100

    reproc: Fix and simplify io test.
    When writing to both stdout and stderr, we wrongly assumed all output
    from stdout would be read before all the output from stderr. Now, we
    always write to both stdout and stderr and capture the streams in
    separate sinks so the comparisons always succeed regardless of the order
    data is read from the streams.

--- reproc/test/io.c
+++ reproc/test/io.c
@@ -5,37 +5,44 @@
 
 #include <string.h>
 
-static void io(const char *mode, const char *input, const char *expected)
+#define MESSAGE "reproc stands for REdirected PROCess"
+
+static void io()
 {
   int r = -1;
 
   reproc_t *process = reproc_new();
   assert(process);
 
-  const char *argv[3] = { RESOURCE_DIRECTORY "/io", mode, NULL };
+  const char *argv[3] = { RESOURCE_DIRECTORY "/io", NULL };
 
   r = reproc_start(process, argv, (reproc_options){ 0 });
   assert(r >= 0);
 
-  r = reproc_write(process, (uint8_t *) input, strlen(input));
+  r = reproc_write(process, (uint8_t *) MESSAGE, strlen(MESSAGE));
   assert(r == 0);
 
   r = reproc_close(process, REPROC_STREAM_IN);
   assert(r == 0);
 
-  char *output = NULL;
-  reproc_sink sink = reproc_sink_string(&output);
-  r = reproc_drain(process, sink, sink);
+  char *out = NULL;
+  char *err = NULL;
+  r = reproc_drain(process, reproc_sink_string(&out), reproc_sink_string(&err));
   assert(r == 0);
-  assert(output != NULL);
 
-  assert(strcmp(output, expected) == 0);
+  assert(out != NULL);
+  assert(err != NULL);
+
+  assert(strcmp(out, MESSAGE) == 0);
+  assert(strcmp(err, MESSAGE) == 0);
 
   r = reproc_wait(process, REPROC_INFINITE);
   assert(r == 0);
 
   reproc_destroy(process);
-  reproc_free(output);
+
+  reproc_free(out);
+  reproc_free(err);
 }
 
 static void timeout(void)
@@ -63,13 +70,8 @@ static void timeout(void)
   reproc_destroy(process);
 }
 
-#define MESSAGE "reproc stands for REdirected PROCess"
-
 int main(void)
 {
-  io("stdout", MESSAGE, MESSAGE);
-  io("stderr", MESSAGE, MESSAGE);
-  io("both", MESSAGE, MESSAGE MESSAGE);
-
+  io();
   timeout();
 }
