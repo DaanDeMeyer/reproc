72d705a2bc0900e21a8947e2405f4dde76e27bb1
Daan De Meyer <daan.j.demeyer@gmail.com>
2020-01-17 18:52:37 +0100

    reproc: Improve test logging.

--- reproc/test/assert.h
+++ reproc/test/assert.h
@@ -4,8 +4,17 @@
 #include <stdlib.h>
 
 #define assert(expression)                                                     \
-  if (!(expression)) {                                                         \
-    fprintf(stderr, "%s:%u: Assertion '%s' failed.", __FILE__, __LINE__,       \
-            #expression);                                                      \
-    exit(1);                                                                   \
-  }
+  do {                                                                         \
+    if (!(expression)) {                                                       \
+      fprintf(stderr, "%s:%u: Assertion '%s' failed ", __FILE__, __LINE__,     \
+              #expression);                                                    \
+                                                                               \
+      if (r < 0) {                                                             \
+        fprintf(stderr, "(r == %s)", reproc_strerror(r));                      \
+      }                                                                        \
+                                                                               \
+      fflush(stderr);                                                          \
+                                                                               \
+      exit(EXIT_FAILURE);                                                      \
+    }                                                                          \
+  } while (0)
