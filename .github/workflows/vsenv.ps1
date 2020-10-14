6cf99d93ab322942155bdbf565633b249c4f36ce
Daan De Meyer <daan.j.demeyer@gmail.com>
2020-10-14 20:32:14 +0100

    CI: Fix call to Add-Content in vsenv.ps1

--- .github/workflows/vsenv.ps1
+++ .github/workflows/vsenv.ps1
@@ -11,5 +11,5 @@ $command = "$vsDevCmdPath -no_logo -arch=$arch -host_arch=$hostArch"
 # https://github.com/microsoft/vswhere/wiki/Start-Developer-Command-Prompt
 & "${env:COMSPEC}" /s /c "$command && set" | ForEach-Object {
   $name, $value = $_ -split '=', 2
-  Add-Content "$name=$value" $env:GITHUB_ENV -Encoding utf8
+  Add-Content -Path $env:GITHUB_ENV -Encoding utf8 "$name=$value"
 }
